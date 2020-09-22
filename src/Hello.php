c8a4c1c55cd452a6a4ef616e131ea3799db00f73
damiao <damiao.microdreams.com>
2020-09-22 18:04:46 +0800

    x

---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uuui
+ * Date: 2020/9/22
+ * Time: 17:43
+ */
+
+namespace iiiuWechatTools;
+
+
+class Hello
+{
+    
+    public function __construct()
+    {
+    }
+}
